38c3dc1b216ac0be765a75784c8e574fce270bda
Karan Munjani <karanmunjani0521@gmail.com>
2020-12-23 08:42:50 +0530

    handling Multiple input with single source of truth, using value as prop from state

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
+  username = React.createRef();
+  //   Avoid using ref  for form data, but if you need to work with dom for use of animation library,3rd party dom libs then use it
+
   handleSubmit = (e) => {
     e.preventDefault();
+
     // call to server and submit data if logged in then redirect movies page
+    // const userName = this.username.current.value;
+    // console.log(userName);
 
     console.log("submitted");
   };
+
+  handleInput = (e) => {
+    console.log(e.currentTarget.value);
+    const account = { ...this.state.account };
+    console.log(account);
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>LogIn</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <label htmlFor="e-mail">Email</label>
+            <label htmlFor="username">Email</label>
             <input
               type="text"
-              name="e-mail"
-              id="e-mail"
+              name="username"
+              value={account.username}
+              onChange={this.handleInput}
+              id="username"
+              autoFocus
               className="form-control"
             />
           </div>
@@ -25,7 +50,9 @@ class LoginForm extends Component {
             <label htmlFor="password">Password</label>
             <input
               type="password"
-              name="passsword"
+              value={account.password}
+              onChange={this.handleInput}
+              name="password"
               id="password"
               className="form-control"
             />
